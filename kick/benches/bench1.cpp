b56d92f5b38f739ba0841e75574812103fffcebc
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2017-11-09 10:40:37 +0100

    finished the kick bench

--- kick/benches/bench1.cpp
+++ kick/benches/bench1.cpp
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "interp_kick.h"
+#include "kick.h"
 #include "utils.h"
 #include <vector>
 #include <random>
@@ -11,14 +11,14 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int n_turns = 50000;
+    int n_turns = 5000;
     int n_particles = 1000000;
-    int n_slices = 1000;
+    int n_rf = 4;
     int n_threads = 1;
 
     if (argc > 1) n_turns = atoi(argv[1]);
     if (argc > 2) n_particles = atoi(argv[2]);
-    if (argc > 3) n_slices = atoi(argv[3]);
+    if (argc > 3) n_rf = atoi(argv[3]);
     if (argc > 4) n_threads = atoi(argv[4]);
     omp_set_num_threads(n_threads);
 
@@ -28,8 +28,8 @@ int main(int argc, char const *argv[])
 
     // initialize variables
     vector<double> dE, dt;
-    vector<double> voltage, edges, bin_centers;
-    double cut_left, cut_right, acc_kick;
+    vector<double> voltage, omega_rf, phi_rf;
+    double acc_kick;
 
     dE.resize(n_particles); dt.resize(n_particles);
     for (int i = 0; i < n_particles; ++i) {
@@ -37,30 +37,23 @@ int main(int argc, char const *argv[])
         dt[i] = 10e-6 * d(gen);
     }
 
-    voltage.resize(n_slices);
-    for (int i = 0; i < n_slices; ++i) {
+    voltage.resize(n_rf);
+    omega_rf.resize(n_rf);
+    phi_rf.resize(n_rf);
+    for (int i = 0; i < n_rf; ++i) {
         voltage[i] = d(gen);
+        omega_rf[i] = d(gen);
+        phi_rf[i] = d(gen);
     }
-    cut_left = dt[rand() % n_slices];
-    cut_right = dt[rand() % n_slices];
     acc_kick = 10e6 * d(gen);
-    if (cut_left > cut_right) swap(cut_left, cut_right);
-
-    edges.resize(n_slices);
-    linspace(cut_left, cut_right, n_slices + 1, edges.data());
-
-    bin_centers.resize(n_slices);
-    for (int i = 0; i < n_slices; ++i) {
-        bin_centers[i] = (edges[i] + edges[i + 1]) / 2.;
-    }
 
     auto papiprof = new PAPIProf();
-    papiprof->start_counters("interp_kick");
+    papiprof->start_counters("kick");
     // main loop
     for (int i = 0; i < n_turns; ++i) {
-        linear_interp_kick_v0(dt.data(), dE.data(), voltage.data(),
-                              bin_centers.data(), n_slices, n_particles,
-                              acc_kick);
+        kick_v0(dt.data(), dE.data(), n_rf,
+                voltage.data(), omega_rf.data(), phi_rf.data(),
+                n_particles, acc_kick);
     }
     papiprof->stop_counters();
     papiprof->report_timing();
